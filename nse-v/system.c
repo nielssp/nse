0b425a67b2d827e5d162aad81fe6335e6468d1c3
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2019-04-30 17:51:28 +0200

    Product, division and less-than/

--- nse-v/system.c
+++ nse-v/system.c
@@ -7,6 +7,7 @@
 #include "module.h"
 #include "../src/util/stream.h"
 #include "eval.h"
+#include "lang.h"
 
 #include "system.h"
 
@@ -112,6 +113,183 @@ static Value num_subtract(Slice args, Scope *dynamic_scope) {
   delete_slice(args);
   return result;
 }
+
+static Value nothing_product(Slice args, Scope *dynamic_scope) {
+  delete_slice(args);
+  return I64(1);
+}
+
+static Value i64_product(Slice args, Scope *dynamic_scope) {
+  int64_t acc = 1;
+  for (size_t i = 0; i < args.length; i++) {
+    Value h = args.cells[i];
+    if (h.type == VALUE_I64) {
+      acc *= h.i64;
+    } else {
+      delete_slice(args);
+      set_debug_arg_index(i);
+      raise_error(domain_error, "expected i64");
+      return undefined;
+    }
+  }
+  delete_slice(args);
+  return I64(acc);
+}
+
+static Value num_product(Slice args, Scope *dynamic_scope) {
+  double acc = 1.0;
+  for (size_t i = 0; i < args.length; i++) {
+    Value h = args.cells[i];
+    if (h.type == VALUE_I64) {
+      acc *= h.i64;
+    } else if (h.type == VALUE_F64) {
+      acc *= h.f64;
+    } else {
+      delete_slice(args);
+      set_debug_arg_index(i);
+      raise_error(domain_error, "expected number");
+      return undefined;
+    }
+  }
+  delete_slice(args);
+  return F64(acc);
+}
+
+static Value i64_divide(Slice args, Scope *dynamic_scope) {
+  Value result = undefined;
+  if (args.length == 0) {
+    raise_error(domain_error, "too few parameters");
+  } else if (args.cells[0].type != VALUE_I64) {
+    set_debug_arg_index(0);
+    raise_error(domain_error, "expected i64");
+  } else {
+    int64_t acc = args.cells[0].i64;
+    if (args.length > 1) {
+      for (size_t i = 1; i < args.length; i++) {
+        Value h = args.cells[i];
+        if (h.type == VALUE_I64) {
+          acc /= h.i64;
+        } else {
+          delete_slice(args);
+          set_debug_arg_index(i);
+          raise_error(domain_error, "expected i64");
+          return undefined;
+        }
+      }
+      result = I64(acc);
+    } else {
+      result = I64(1 / acc);
+    }
+  }
+  delete_slice(args);
+  return result;
+}
+
+static Value num_divide(Slice args, Scope *dynamic_scope) {
+  Value result = undefined;
+  if (args.length == 0) {
+    raise_error(domain_error, "too few parameters");
+  } else if (args.cells[0].type != VALUE_I64 && args.cells[0].type != VALUE_F64) {
+    set_debug_arg_index(0);
+    raise_error(domain_error, "expected number");
+  } else {
+    double acc = args.cells[0].type == VALUE_I64 ? args.cells[0].i64 : args.cells[0].f64;
+    if (args.length > 1) {
+      for (size_t i = 1; i < args.length; i++) {
+        Value h = args.cells[i];
+        if (h.type == VALUE_I64) {
+          acc /= h.i64;
+        } else if (h.type == VALUE_F64) {
+          acc /= h.f64;
+        } else {
+          delete_slice(args);
+          set_debug_arg_index(i);
+          raise_error(domain_error, "expected number");
+          return undefined;
+        }
+      }
+      result = F64(acc);
+    } else {
+      result = F64(1.0 / acc);
+    }
+  }
+  delete_slice(args);
+  return result;
+}
+
+static Value i64_less_than(Slice args, Scope *dynamic_scope) {
+  Value result = undefined;
+  if (args.length == 0) {
+    raise_error(domain_error, "too few parameters");
+  } else if (args.cells[0].type != VALUE_I64) {
+    set_debug_arg_index(0);
+    raise_error(domain_error, "expected i64");
+  } else {
+    int64_t previous = args.cells[0].i64;
+    if (args.length > 1) {
+      for (size_t i = 1; i < args.length; i++) {
+        Value h = args.cells[i];
+        if (h.type == VALUE_I64) {
+          if (h.i64 > previous) {
+            previous = h.i64;
+          } else {
+            delete_slice(args);
+            return FALSE;
+          }
+        } else {
+          delete_slice(args);
+          set_debug_arg_index(i);
+          raise_error(domain_error, "expected i64");
+          return undefined;
+        }
+      }
+    }
+    result = TRUE;
+  }
+  delete_slice(args);
+  return result;
+}
+
+static Value num_less_than(Slice args, Scope *dynamic_scope) {
+  Value result = undefined;
+  if (args.length == 0) {
+    raise_error(domain_error, "too few parameters");
+  } else if (args.cells[0].type != VALUE_I64 && args.cells[0].type != VALUE_F64) {
+    set_debug_arg_index(0);
+    raise_error(domain_error, "expected number");
+  } else {
+    double previous = args.cells[0].type == VALUE_I64 ? args.cells[0].i64 : args.cells[0].f64;
+    if (args.length > 1) {
+      for (size_t i = 1; i < args.length; i++) {
+        Value h = args.cells[i];
+        if (h.type == VALUE_I64) {
+          if (h.i64 > previous) {
+            previous = h.i64;
+          } else {
+            delete_slice(args);
+            return FALSE;
+          }
+        } else if (h.type == VALUE_F64) {
+          if (h.f64 > previous) {
+            previous = h.f64;
+          } else {
+            delete_slice(args);
+            return FALSE;
+          }
+        } else {
+          delete_slice(args);
+          set_debug_arg_index(i);
+          raise_error(domain_error, "expected number");
+          return undefined;
+        }
+      }
+    }
+    result = TRUE;
+  }
+  delete_slice(args);
+  return result;
+}
+
 
 static Value append(Slice args, Scope *dynamic_scope) {
   size_t length = 0;
@@ -220,7 +398,7 @@ static Value string_elem(Slice args, Scope *dynamic_scope) {
       result = I64(TO_STRING(args.cells[1])->bytes[args.cells[0].i64]);
     } else {
       set_debug_arg_index(0);
-      raise_error(domain_error, "index out of bounds");
+      raise_error(domain_error, "index out of bounds: %ld", args.cells[0].i64);
     }
   } else {
     raise_error(domain_error, "expected (elem INT STRING)");
@@ -238,7 +416,7 @@ static Value vector_elem(Slice args, Scope *dynamic_scope) {
       result = copy_value(TO_VECTOR(args.cells[1])->cells[args.cells[0].i64]);
     } else {
       set_debug_arg_index(0);
-      raise_error(domain_error, "index out of bounds");
+      raise_error(domain_error, "index out of bounds: %ld", args.cells[0].i64);
     }
   } else {
     raise_error(domain_error, "expected (elem INT VECTOR)");
@@ -256,7 +434,7 @@ static Value vector_slice_elem(Slice args, Scope *dynamic_scope) {
       result = copy_value(TO_VECTOR_SLICE(args.cells[1])->cells[args.cells[0].i64]);
     } else {
       set_debug_arg_index(0);
-      raise_error(domain_error, "index out of bounds");
+      raise_error(domain_error, "index out of bounds: %ld", args.cells[0].i64);
     }
   } else {
     raise_error(domain_error, "expected (elem INT VECTOR-SLICE)");
@@ -286,6 +464,26 @@ Module *get_system_module() {
   module_ext_define_method(system, "-", FUNC(i64_subtract),
       1, copy_type(i64_type));
   module_ext_define_method(system, "-", FUNC(num_subtract),
+      1, copy_type(num_type));
+
+  module_ext_define_generic(system, "*", 0, 1, 1, (int8_t[]){ 0 });
+  module_ext_define_method(system, "*", FUNC(nothing_product),
+      1, copy_type(nothing_type));
+  module_ext_define_method(system, "*", FUNC(i64_product),
+      1, copy_type(i64_type));
+  module_ext_define_method(system, "*", FUNC(num_product),
+      1, copy_type(num_type));
+
+  module_ext_define_generic(system, "/", 1, 1, 1, (int8_t[]){ 0, 0 });
+  module_ext_define_method(system, "/", FUNC(i64_divide),
+      1, copy_type(i64_type));
+  module_ext_define_method(system, "/", FUNC(num_divide),
+      1, copy_type(num_type));
+
+  module_ext_define_generic(system, "<", 1, 1, 1, (int8_t[]){ 0, 0 });
+  module_ext_define_method(system, "<", FUNC(i64_less_than),
+      1, copy_type(i64_type));
+  module_ext_define_method(system, "<", FUNC(num_less_than),
       1, copy_type(num_type));
 
   module_ext_define_generic(system, "length", 1, 0, 1, (int8_t[]){ 0 });
